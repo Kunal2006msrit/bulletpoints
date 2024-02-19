210e21b94df02aa8f64e52ba7508b9164a661340
Your NameKunal <you@example.comkunal.cimfr20@gmail.com>
2024-02-19 18:19:55 +0530

    updated repo

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages,setup
 setup(name="Bullet Point Generator",
       version="0.0.1",
-      author="Kunal Saurabh"
-      email="kunal.cimfr20@gmail.com",
+      author="Kunal Saurabh",
+      author_email="kunal.cimfr20@gmail.com",
       install_requires=["openai","langchain","streamlit","python-dotenv","pyPDF2"],
       packages=find_packages()
 )
